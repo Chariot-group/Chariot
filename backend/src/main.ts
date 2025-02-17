57fb326f4176f0f506bbe4cc85928dfd2cb6ed8a
Elvis Pichou <elvis@mac.home>
2025-02-17 19:39:03 +0100

    🗃️ DB(): Import globally mongoose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Chariot API running on port:', process.env.API_PORT ?? 3000);
   await app.listen(process.env.API_PORT ?? 3000);
+
+  console.log('Chariot API running on port:', process.env.API_PORT ?? 3000);
 }
 bootstrap();
