c8582e48a1c1cb87aa71c1bc26b0094094cb1d58
Elvis Pichou <56622967+Eyvos@users.noreply.github.com>
2025-02-20 21:31:15 +0100

    21 api configuration mongodb (#29)
    * ➕ Chores(package): Add nest config
    
    * 🔧 Build(app): Add config module in app
    
    * 🏗️ Build(main): Handle .env in API
    
    * ➕ Chores(dependencies): Add mongoose
    
    * 🗃️ DB(): Import globally mongoose
    
    * 📝 Docs(Readme): add readme
    
    ---------
    
    Co-authored-by: Elvis Pichou <elvis@mac.home>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Permet d'accéder aux variables du env dans tous les modules
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
