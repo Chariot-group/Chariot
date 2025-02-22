4c6b8fc88833ff05c3ebad2ee83aa8b354103e6e
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-02-22 21:06:47 +0100

    Add logger

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Logger, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly logger: Logger
+  ) {}
+
+  readonly SERVICE = AppController.name;
 
   @Get()
   getHello(): string {
+    this.logger.log("Lancement du serveur 'info'", this.SERVICE);
+    this.logger.debug("Lancement du serveur 'info'", this.SERVICE);
+    this.logger.error("Lancement du serveur 'info'", this.SERVICE);
+    this.logger.fatal("Lancement du serveur 'info'", this.SERVICE);
+    this.logger.verbose("Lancement du serveur 'info'", this.SERVICE);
+    this.logger.warn("Lancement du serveur 'info'", this.SERVICE);
     return this.appService.getHello();
   }
 }
