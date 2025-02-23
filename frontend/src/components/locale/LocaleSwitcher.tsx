9b38a35506053ac27b975f1633b06faacb1f53a4
Elvis Pichou <56622967+Eyvos@users.noreply.github.com>
2025-02-23 13:40:05 +0100

    24 front mise en place dun fichier de traduction (#31)
    * ➕ Chores(package): Add next-intl
    
    * 🌐 Chores(i18n): Add fr, en and es support
    
    * ✨ Feat(i18n): Setup next-intl in the project
    
    * 🐛 Fix(i18n): Fix async console warning
    
    * ➕ Chores(package): Add shadcn + select component
    
    * ✨ Feat(i18n): Add localeswitcher
    
    * 📝 Docs(i18n): Add i18n.md
    
    * 🔥 Refactor(i18n): Remove useless key in the LocaleSwitcher translation files
    
    * 📝 Docs(i18n): Correct mistake
    
    * 🔨 Chores(i18n): Add i18n scripts
    
    * 🔨 Chores(i18n): Add automation on locale add
    
    * 📝 Docs(i18n): Add i18n language add docs
    
    ---------
    
    Co-authored-by: Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>
    Co-authored-by: Elvis Pichou <elvis@mac.home>

---
+++ frontend/src/components/locale/LocaleSwitcher.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useLocale, useTranslations } from "next-intl";
+
+export default function LanguageSwitcher() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // Récupérer la locale courante
+  const currentLocale = useLocale();
+  const t = useTranslations("LocaleSwitcher");
+
+  const handleChange = (newLocale: string) => {
+    const pathSegments = pathname.split("/");
+    pathSegments[1] = newLocale; // Mise à jour de la locale dans l'URL
+    const newPath = pathSegments.join("/");
+    router.push(newPath);
+  };
+
+  return (
+    <Select onValueChange={handleChange} value={currentLocale}>
+      <SelectTrigger className="w-[150px]">
+        <SelectValue placeholder="Select language" />
+      </SelectTrigger>
+      <SelectContent>
+        {routing.locales.map((current) => (
+          <SelectItem key={current} value={current}>
+            {t("locale", { locale: current })}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
