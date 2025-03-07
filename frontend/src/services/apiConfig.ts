e93096ec61a6b9a41f8b1de5afb5a2b901c0aba6
Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>
2025-03-07 11:31:40 +0100

    🥅 Refactor(ApiConfig): Handle no url found error

--- frontend/src/services/apiConfig.ts
+++ frontend/src/services/apiConfig.ts
@@ -4,6 +4,9 @@ import axios from "axios";
 const url = process.env.NEXT_PUBLIC_API_URL;
 
 const apiClient = (contentType: string) => {
+  if (!url) {
+    throw new Error("API URL is not defined");
+  }
   const instance = axios.create({
     baseURL: url,
     headers: {
