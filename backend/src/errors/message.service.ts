44b282941c7a30a285a6eeece7c78194abc6f1bc
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-03-24 16:01:20 +0100

    Use service for log

--- backend/src/errors/message.service.ts
+++ backend/src/errors/message.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ErrorsService {
+export class MessageService {
 
     private readonly errors: {code: string, message: string}[] = [
         {
-            code: "resource_not_find",
+            code: "external_resource_not_find",
             message: "Impossible de trouver {?} #${?}"
         },
         {
-            code: "resource_find",
+            code: "external_resource_find",
             message: "{?} #${?} trouvé avec succès"
         },
         {
-            code: "mongoose_id_error",
+            code: "external_mongoose_id_error",
             message: "Erreur lors de la récupération de la campagne #${?}: L'id n'est pas un id valide pour mongoose"
+        },
+        {
+            code: "internal_resource_not_find",
+            message: "Erreur lors de la récupération de {?} #${?}: ${?}"
+        },
+        {
+            code: "internal_resource_find",
+            message: "Erreur lors de la récupération de {?} #${?}: ${?}"
         }
     ];
 
