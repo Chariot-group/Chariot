f10d52fee369d4360a6abafd9cd41a8ce4690138
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-03-26 20:01:23 +0100

    Update user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class UserService {
+
+    constructor(
+      @InjectModel(User.name) private userModel: Model<UserDocument>,
+    ) {}
+  
+    private readonly SERVICE_NAME = UserService.name;
+    private readonly logger = new Logger(this.SERVICE_NAME);
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -16,8 +27,41 @@ export class UserService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    try{
+      if (!Types.ObjectId.isValid(id)) {
+        const message = `Error while updating user #${id}: Id is not a valid mongoose id`;
+        this.logger.error(message, null, this.SERVICE_NAME);
+        throw new BadRequestException(message);
+      }
+
+      const start: number = Date.now();
+      const userUpdate = await this.userModel
+        .updateOne({ _id: id }, updateUserDto)
+        .exec();
+      const user = await this.userModel
+        .findById(id)
+        .populate('campaigns')
+        .exec();
+      const end: number = Date.now();
+      
+      if (!user || userUpdate.modifiedCount === 0) {
+        const message = `User #${id} not found`;
+        this.logger.error(message, null, this.SERVICE_NAME);
+        throw new NotFoundException(message);
+      }
+      
+      const message = `User update in ${end - start}ms`;
+      this.logger.verbose(message, this.SERVICE_NAME);
+      return {
+        message,
+        data: user,
+      };
+    }catch(error){
+      const message = `Error while updating user: ${error.message}`;
+      this.logger.error(message, null, this.SERVICE_NAME);
+      throw new InternalServerErrorException(message);
+    }
   }
 
   remove(id: number) {
