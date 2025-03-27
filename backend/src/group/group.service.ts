693e2eab754778a53b87dda52d9e9635717f6a27
Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>
2025-03-27 14:34:32 +0100

    ✨ Feat(group):Add Character schema to group module and handle deleted groups in service

--- backend/src/group/group.service.ts
+++ backend/src/group/group.service.ts
@@ -1,19 +1,31 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  GoneException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Group, GroupDocument } from './schemas/group.schema';
 import { Model, Types } from 'mongoose';
+import {
+  Character,
+  CharacterDocument,
+} from '@/character/schemas/character.schema';
 
 @Injectable()
 export class GroupService {
+  constructor(
+    @InjectModel(Group.name) private groupModel: Model<GroupDocument>,
+    @InjectModel(Character.name)
+    private characterModel: Model<CharacterDocument>,
+  ) {}
 
-    constructor(
-      @InjectModel(Group.name) private groupModel: Model<GroupDocument>,
-    ) {}
-  
-    private readonly SERVICE_NAME = GroupService.name;
-    private readonly logger = new Logger(this.SERVICE_NAME);
+  private readonly SERVICE_NAME = GroupService.name;
+  private readonly logger = new Logger(this.SERVICE_NAME);
 
   create(createGroupDto: CreateGroupDto) {
     return 'This action adds a new group';
@@ -32,37 +44,56 @@ export class GroupService {
   }
 
   async remove(id: string) {
-    try{
-
+    try {
       if (!Types.ObjectId.isValid(id)) {
         const message = `Error while deleting group #${id}: Id is not a valid mongoose id`;
         this.logger.error(message, null, this.SERVICE_NAME);
         throw new BadRequestException(message);
       }
 
-      const start: number = Date.now();
       const group = await this.groupModel.findById(id).exec();
 
-      if(!group){
+      if (!group) {
         const message = `Error while deleting group #${id}: Group not found`;
         this.logger.error(message, null, this.SERVICE_NAME);
         throw new NotFoundException(message);
       }
 
-      group.characters.forEach(async character => {
-        console.log(character);
-        //TODO: delete character
-      });
+      if (group.deletedAt) {
+        const message = `Error while deleting group #${id}: Group already deleted`;
+        this.logger.error(message, null, this.SERVICE_NAME);
+        throw new GoneException(message);
+      }
+
+      const start: number = Date.now();
+      group.deletedAt = new Date();
+
+      const updated = await this.characterModel
+        .updateMany(
+          { _id: { $in: group.characters } },
+          { $pull: { groups: id } },
+        )
+        .exec();
+
+      group.characters = [];
+      group.save();
 
       const end: number = Date.now();
+
       const message = `Group delete in ${end - start}ms`;
       this.logger.verbose(message, this.SERVICE_NAME);
       return {
         message,
         data: group,
       };
-
-    }catch(error){
+    } catch (error) {
+      if (
+        error instanceof BadRequestException ||
+        error instanceof NotFoundException ||
+        error instanceof GoneException
+      ) {
+        throw error;
+      }
       const message = `Error while deleting group #${id}: ${error.message}`;
       this.logger.error(message, null, this.SERVICE_NAME);
       throw new InternalServerErrorException(message);
