c24b6dccf162715551c10ddd21a1bc63cdd28abf
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-03-27 14:52:20 +0100

    Update user (#70)
    * Update user
    
    * 🥅 Refactor(users): Add validation for user update and improve error handling
    
    ---------
    
    Co-authored-by: Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,15 @@
+import { IsArray, IsMongoId, IsString, MaxLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @IsString()
+    @MaxLength(255)
+    readonly username?: string;
+
+    @IsArray()
+    @IsMongoId({ each: true })
+    readonly campaigns?: string[];
+
+}
