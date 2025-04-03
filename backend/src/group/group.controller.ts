0b6148a50513ce8443471d559539eda44c3e3c24
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-03 09:16:16 +0200

    Supprimer l'importation de ValidationPipe dans le contrôleur de groupe

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
