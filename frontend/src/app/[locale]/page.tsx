1eb46ce6d45aca016d8286d5fb8d35f93eaa3687
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-12 18:23:00 +0200

    Change param

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import GroupListPanel from "@/components/modules/groups/GroupListPanel";
 import { IGroup } from "@/models/groups/IGroup";
 import ICharacter from "@/models/characters/ICharacter";
 import { useState } from "react";
+import GroupsCampaignsPanel from "@/components/modules/campaigns/GroupsCampaignsPanel";
 
 export default function Home() {
 
@@ -17,8 +18,8 @@ export default function Home() {
     
     
     <div className="flex flex-row">
-      <div>
-        
+      <div className="w-[70%] p-6 h-[50vh]">
+        <GroupsCampaignsPanel idCampaign="67f8edf28b4d04f3d0c07aed"/>
       </div>
     </div>
   );
--- frontend/src/components/modules/campaigns/GroupsCampaignsPanel.tsx
+++ frontend/src/components/modules/campaigns/GroupsCampaignsPanel.tsx
@@ -1,14 +1,48 @@
-import { useLocale, useTranslations } from "next-intl";
+"use client"
 
-export default function GroupsCampaignsPanel() {
+import { Button } from "@/components/ui/button";
+import { useLocale, useTranslations } from "next-intl";
+import GroupListPanel from "../groups/GroupListPanel";
+import { IGroup } from "@/models/groups/IGroup";
+import { useState } from "react";
+
+interface Props {
+    idCampaign: string; // ID de la campagne des groupes
+}
+export default function GroupsCampaignsPanel({ idCampaign }: Props) {
 
     const currentLocale = useLocale();
-    const t = useTranslations("CharacterListPanel");
+    const t = useTranslations("GroupListPanel");
+
+    const [group, setGroupSelected] = useState<IGroup | null>(null);
+
+    const handleGroupDrop = (group: IGroup, targetType: string) => {
+        console.log(`Groupe déplacé : ${group.label}, vers la liste : ${targetType}`);
+        // Ajoutez ici la logique pour mettre à jour les données côté serveur ou local
+    };
 
     return(
         <div className="w-full h-full flex flex-col">
-            <div className="flex">
-                <h2>{}</h2>
+            <div className="flex flex-row justify-between items-center">
+                <h2>{t("title.default")}</h2>
+                <Button >{t("create")}</Button>
+            </div>
+            <div className="flex flex-row gap-4 mt-4 justify-between h-full">
+                <div className="rounded-xl border border-ring bg-card text-card-foreground shadow">
+                    <GroupListPanel reverse={true} grabbled={true} idCampaign={idCampaign} addable={false} type="main" groupSelected={group} setGroupSelected={setGroupSelected} />
+                </div>
+                <div className="flex items-center">
+                    <span className="text-muted-foreground">{"< >"}</span>
+                </div>
+                <div className="rounded-xl border border-ring bg-card text-card-foreground shadow">
+                    <GroupListPanel reverse={true} grabbled={true} idCampaign={idCampaign} addable={false} type="npc" groupSelected={group} setGroupSelected={setGroupSelected} />
+                </div>
+                <div className="flex items-center">
+                    <span className="text-muted-foreground">{"< >"}</span>
+                </div>
+                <div className="rounded-xl border border-ring bg-card text-card-foreground shadow">
+                    <GroupListPanel reverse={true} grabbled={true} idCampaign={idCampaign} addable={false} type="archived" groupSelected={group} setGroupSelected={setGroupSelected} />
+                </div>
             </div>
         </div>
     );
