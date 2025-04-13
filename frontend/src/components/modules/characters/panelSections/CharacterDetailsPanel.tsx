0a4d15207c33620f68dc3d8345adfccd73f37b3f
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-13 22:02:22 +0200

    Actions sections

--- frontend/src/components/modules/characters/panelSections/CharacterDetailsPanel.tsx
+++ frontend/src/components/modules/characters/panelSections/CharacterDetailsPanel.tsx
@@ -9,6 +9,10 @@ import { useEffect, useRef, useState } from "react";
 import GlobalSection from "./global/GlobalSection";
 import IClassification from "@/models/characters/classification/IClassification";
 import CombatSection from "./combat/CombatSection";
+import IStats from "@/models/characters/stat/IStats";
+import ICombat from "@/models/characters/combat/ICombat";
+import IActions from "@/models/characters/actions/IActions";
+import ActionsSection from "./actions/ActionsSection";
 
 interface ICharacterDetailsPanelProps {
     character: ICharacter;
@@ -20,8 +24,9 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
     const cancelRef = useRef<boolean>(false);
     const [name, setName] = useState<string>(character.name);
     const [classification, setClassification] = useState<IClassification>(character.classification);
-    const [stats, setStats] = useState(character.stats);
-    const [combat, setCombat] = useState(character.combat);
+    const [stats, setStats] = useState<IStats>(character.stats);
+    const [combat, setCombat] = useState<ICombat>(character.combat);
+    const [action, setAction] = useState<IActions>(character.actions[0]);
 
     useEffect(() => {
         cancelRef.current = true;
@@ -29,6 +34,7 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
         setClassification(character.classification);
         setStats(character.stats);
         setCombat(character.combat);
+        setAction(character.actions[0]);
         // Attendre que le composant soit monté avant de mettre à jour le state
         (async () => {
             await new Promise(resolve => setTimeout(resolve, 0));
@@ -38,12 +44,12 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
 
     const [global, setGlobal] = useState<boolean>(true);
     const [combatNav, setCombatNav] = useState<boolean>(false);
-    const [actions, setActions] = useState<boolean>(false);
+    const [actionsNav, setActionsNav] = useState<boolean>(false);
     const [traits, setTraits] = useState<boolean>(false);
     const updateTab = (tab: "global" | "combat" | "actions" | "traits") => {
         setGlobal(tab === "global");
         setCombatNav(tab === "combat");
-        setActions(tab === "actions");
+        setActionsNav(tab === "actions");
         setTraits(tab === "traits");
     }
 
@@ -52,12 +58,13 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
         character.classification = classification;
         character.stats = stats;
         character.combat = combat;
+        character.actions[0] = action;
         console.log(character);
     }
     useEffect(() => {
         if (cancelRef.current) return;
         onChange();
-    }, [classification, stats, combat]);
+    }, [classification, stats, combat, action]);
 
   return (
     <Card className="flex flex-col h-full gap-3 p-5">
@@ -68,10 +75,10 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
             <div className="flex flex-row items-center gap-2 text-foreground">
                 <span className={`cursor-pointer underline-offset-4 ${global ? "underline" : "hover:underline"}`} onClick={(e) => updateTab('global')}>{t("navigation.global")}</span>
                 <span className={`cursor-pointer underline-offset-4 ${combatNav ? "underline" : "hover:underline"}`} onClick={(e) => updateTab('combat')}>{t("navigation.combat")}</span>
-                <span className={`cursor-pointer underline-offset-4 ${actions ? "underline" : "hover:underline"}`} onClick={(e) => updateTab('actions')}>{t("navigation.actions")}</span>
+                <span className={`cursor-pointer underline-offset-4 ${actionsNav ? "underline" : "hover:underline"}`} onClick={(e) => updateTab('actions')}>{t("navigation.actions")}</span>
                 <span className={`cursor-pointer underline-offset-4 ${traits ? "underline" : "hover:underline"}`} onClick={(e) => updateTab('traits')}>{t("navigation.traits")}</span>
             </div>
-            <Button variant={"link"}>Supprimer</Button>
+            <Button variant={"link"}>{t("actions.characterDelete")}</Button>
         </div>
         <div className="flex flex-col h-full flex-1 h-auto overflow-auto scrollbar-hide">
             {
@@ -85,8 +92,8 @@ export function CharacterDetailsPanel({ character }: ICharacterDetailsPanelProps
                 )
             }
             {
-                actions && (
-                    <h1>Actions</h1>
+                actionsNav && (
+                    <ActionsSection actions={action} setActions={setAction} />
                 )
             }
             {
---
+++ frontend/src/components/modules/characters/panelSections/actions/ActionsSection.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useTranslations } from "next-intl";
+import IAction from "@/models/characters/actions/IActions";
+import IActions from "@/models/characters/actions/IActions";
+import { Card } from "@/components/ui/card";
+import Champs from "@/components/common/Champs";
+import { ActionCard } from "./cards/ActionCard";
+import { Button } from "@/components/ui/button";
+
+interface IGlobalSectionProps {
+    actions: IActions;
+    setActions: (actions: IActions) => void;
+}
+export default function ActionsSection({ actions, setActions }: IGlobalSectionProps) {
+
+    const t = useTranslations("CharacterDetailsPanel");
+
+    const onDelete = (index: number) => {
+        const newActions = [...actions.standard];
+        newActions.splice(index, 1);
+        setActions({ ...actions, standard: newActions });
+    }
+
+    return (
+        <div className="flex flex-col gap-3">
+            <div className="flex justify-end">
+                <Button onClick={() => setActions({ ...actions, standard: [...actions.standard, { name: "", type: "melee", attackBonus: 0, damage: { type: "bludgeoning", dice: "1d6" }, range: "melee" }].reverse() })}>
+                    {t("actions.actionsAdd")}
+                </Button>
+            </div>
+            {
+                actions.standard.map((action, index) => (
+                    <ActionCard
+                        onDelete={() => onDelete(index)}
+                        key={index}
+                        action={action}
+                        setAction={(action) => {
+                            const newActions = [...actions.standard];
+                            newActions[index] = action;
+                            setActions({ ...actions, standard: newActions });
+                        }}
+                    />
+                ))
+            }
+        </div>
+    )
+}
