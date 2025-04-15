9191b3e88de2496e17b32303eae53584955ae7e5
Elvis Pichou <56622967+Eyvos@users.noreply.github.com>
2025-04-15 17:33:17 +0200

    Dnd dans le groupe des campagnes
    * Start
    
    * Change param
    
    * Structure
    
    * ➕ Chores(dependencies): Add @dnd-kit/core dependency to package.json and package-lock.json
    
    * 🚧 WIP(groups): Implement drag-and-drop functionality for group management and enhance GroupListPanel with new item component
    
    * ✨ Feat(groups): Add drop area message to GroupListPanel and update translations
    
    * ♻️ Refactor(groups): Implement drag-and-drop functionality with GroupDnDWrapper and enhance GroupListPanelItem styling
    
    * ♻️ Refactor(groups): Update GroupsCampaignsPanel to handle drag-and-drop functionality and manage group states, enhancing GroupListPanel integration
    
    ---------
    
    Co-authored-by: Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
    Co-authored-by: Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>

--- frontend/src/components/modules/groups/GroupListPanel.tsx
+++ frontend/src/components/modules/groups/GroupListPanel.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useToast } from "@/hooks/useToast";
 import { IGroup } from "@/models/groups/IGroup";
@@ -10,108 +10,142 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import SearchInput from "@/components/common/SearchBar";
 import Loading from "@/components/common/Loading";
-import { Grip } from "lucide-react";
+import { useDroppable } from "@dnd-kit/core";
+import GroupListPanelItem from "./GroupListPanelItem";
 
 interface Props {
-    offset?: number; // Nombre de groupes à afficher par page
-    idCampaign: string; // ID de la campagne des groupes
-    groupSelected: IGroup | null; // Groupe selectionné
-    setGroupSelected: (group: IGroup | null) => void; // Fonction pour mettre à jour le groupe selectionné
-    reverse?: boolean; // Si vrai, les couleurs de fond sont inversé
-    type?: "all" | "main" | "npc" | "archived"; // Titre de groupe à afficher
-    grabbled?: boolean; // Si vrai, le curseur est en mode grab et une icône de grip est affichée
-    addable?: boolean; // Si vrai, le bouton d'ajout de groupe est affiché
+  groups: IGroup[]; // Liste des groupes à afficher
+  setGroups: React.Dispatch<React.SetStateAction<IGroup[]>>; // Setter de la liste des groupes
+  offset?: number; // Nombre de groupes à afficher par page
+  idCampaign: string; // ID de la campagne des groupes
+  reverse?: boolean; // Si vrai, les couleurs de fond sont inversé
+  type?: "all" | "main" | "npc" | "archived"; // Titre de groupe à afficher
+  grabbled?: boolean; // Si vrai, le curseur est en mode grab et une icône de grip est affichée
+  addable?: boolean; // Si vrai, le bouton d'ajout de groupe est affiché
 }
-export default function GroupListPanel({ offset = 8, idCampaign, groupSelected, setGroupSelected, reverse = false, type = "all", grabbled = false, addable = true }: Props) {
+export default function GroupListPanel({
+  groups,
+  setGroups,
+  offset = 8,
+  idCampaign,
+  reverse = false,
+  type = "all",
+  grabbled = false,
+  addable = true,
+}: Props) {
+  const currentLocal = useLocale();
+  const t = useTranslations("GroupListPanel");
+  const { error } = useToast();
 
-    const currentLocal = useLocale();
-    const t = useTranslations('GroupListPanel');
-    const { error } = useToast();
+  const { setNodeRef, isOver } = useDroppable({
+    id: type, // "main", "npc", etc.
+  });
 
-    const [groups, setGroups] = useState<IGroup[]>([]);
+  //Pagination
+  const [page, setPage] = useState<number>(1);
+  const [search, setSearch] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-    //Pagination
-    const [page, setPage] = useState<number>(1);
-    const [search, setSearch] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(false);
+  //Infinite scroll
+  const cardRef = useRef<HTMLDivElement | null>(null);
 
-    //Infinite scroll
-    const containerRef = useRef<HTMLDivElement | null>(null);
-    const cardRef = useRef<HTMLDivElement | null>(null);
-
-    const fetchGroups = useCallback(async (search: string, nextPage = 1, reset = false) => {
-        if(loading) return;
-        setLoading(true);
-        try {
-            const response = await GroupService.getAllGroups(
-              {
-                page: nextPage,
-                offset,
-                label: search,
-                type,
-              }, idCampaign);
-            if(reset) {
-                setGroups(response.data || []);
-                setGroupSelected(response.data[0]);
-            }else {
-                setGroups((prev) => {
-                  //Fix un bug surement dû au seeder.
-                    return [...prev, ...response.data.filter((newGroup: { _id: string; }) => !prev.some(existingGroup => existingGroup._id === newGroup._id))];
-                });
-            }
-            setPage(nextPage);
-        } catch(err){
-            error(t("error"));
-        } finally {
-            setLoading(false);
+  const fetchGroups = useCallback(
+    async (search: string, nextPage = 1, reset = false) => {
+      if (loading) return;
+      setLoading(true);
+      try {
+        const response = await GroupService.getAllGroups(
+          {
+            page: nextPage,
+            offset,
+            label: search,
+            type,
+          },
+          idCampaign
+        );
+        if (reset) {
+          setGroups(response.data || []);
+        } else {
+          setGroups((prev) => {
+            //Fix un bug surement dû au seeder.
+            return [
+              ...prev,
+              ...response.data.filter(
+                (newGroup: { _id: string }) =>
+                  !prev.some(
+                    (existingGroup) => existingGroup._id === newGroup._id
+                  )
+              ),
+            ];
+          });
         }
-    }, [loading])
+        setPage(nextPage);
+      } catch (err) {
+        error(t("error"));
+      } finally {
+        setLoading(false);
+      }
+    },
+    [loading]
+  );
 
-    useInfiniteScroll(containerRef, fetchGroups, page, loading, search);
+  useInfiniteScroll(cardRef, fetchGroups, page, loading, search);
 
-    useEffect(() => {
-        setGroups([]);
-        fetchGroups(search, 1, true);
-    }, [currentLocal, search]);
+  useEffect(() => {
+    setGroups([]);
+    fetchGroups(search, 1, true);
+  }, [currentLocal, search]);
 
-    return (
-      <div className="w-full h-full flex flex-col">
-        <CardHeader className="flex-none h-auto items-center gap-3">
-          <CardTitle className="text-foreground font-bold">{t(type === "all" ? "title.default" : `title.${type}`)}</CardTitle>
-          <SearchInput
-            value={search}
-            onChange={setSearch}
-            placeholder={t("search")}
-            reverse={reverse}
-          />
-        </CardHeader>
-        <CardContent ref={containerRef} className="flex-1 h-auto overflow-auto scrollbar-hide">
-            <div className="flex flex-col gap-3">
-              {addable && 
-              <Link href="/groups/add" title={t("create")}>
-                <Card
-                  ref={cardRef}
-                  className="bg-primary justify-center flex p-2 gap-3 border-ring hover:border-2 hover:border-primary shadow-md"
-                >
-                  <span className="text-background font-bold">{t("create")}</span>
-                </Card>
-              </Link>}
-              {loading && <Loading />}
-              {groups.length > 0  &&
-                groups.map((group) => (
-                  <Card key={group._id} className={`flex p-2 gap-3 border-ring shadow-md hover:border-2 ${reverse ? "bg-background" : "bg-card"} ${grabbled ? "justify-between cursor-grab" : "justify-center"} ${groupSelected?._id === group._id ? "border-2" : "border"}`} onClick={() => setGroupSelected(group)}>
-                    <span className="text-foreground font-bold">{group.label}</span>
-                    {grabbled && <Grip />}
-                  </Card>
-                ))
-              }
-              {groups.length === 0 && !loading && (
-                <div className="row-start-2 col-span-3 flex items-top justify-center">
-                  <p className="text-gray-500">{t("noGroups")}</p>
-                </div>
-              )}
+  return (
+    <div className="w-full h-full flex flex-col">
+      <CardHeader className="flex-none h-auto items-center gap-3">
+        <CardTitle className="text-foreground font-bold">
+          {t(type === "all" ? "title.default" : `title.${type}`)}
+        </CardTitle>
+        <SearchInput
+          value={search}
+          onChange={setSearch}
+          placeholder={t("search")}
+          reverse={reverse}
+        />
+      </CardHeader>
+      <CardContent
+        ref={cardRef}
+        className={`flex-1 h-auto overflow-auto scrollbar-hide ${
+          isOver ? (reverse ? "bg-primary/10" : "bg-primary/20") : ""
+        }`}
+      >
+        <div className="flex flex-col gap-3" ref={setNodeRef}>
+          {addable && (
+            <Link href="/groups/add" title={t("create")}>
+              <Card className="bg-primary justify-center flex p-2 gap-3 border-ring hover:border-2 hover:border-primary shadow-md">
+                <span className="text-background font-bold">{t("create")}</span>
+              </Card>
+            </Link>
+          )}
+          {loading && <Loading />}
+          {groups.length > 0 &&
+            groups.map((group) => (
+              <GroupListPanelItem
+                key={group._id}
+                group={group}
+                currentPanelType={type}
+                grabbled={grabbled}
+                reverse={reverse}
+              />
+            ))}
+          {groups.length === 0 && !loading && !isOver && (
+            <div className="row-start-2 col-span-3 flex items-top justify-center">
+              <p className="text-gray-500">{t("noGroups")}</p>
             </div>
-          </CardContent>
+          )}
+          {isOver && (
+            <div className="row-start-2 col-span-3 flex items-top justify-center">
+              <p className="text-gray-500">{t("dropHere")}</p>
+            </div>
+          )}
+        </div>
+      </CardContent>
     </div>
-    );
+  );
 }
--- frontend/src/services/groupService.ts
+++ frontend/src/services/groupService.ts
@@ -2,42 +2,54 @@ import { APIContentType } from "@/constants/APIContentType";
 import apiClient from "./apiConfig";
 import { IGroup } from "@/models/groups/IGroup";
 
-const END_POINT = "/campaigns"
+const END_POINT = "/campaigns";
 
 const GroupService = {
-
-    async getAllGroups(query: {page?: number, offset?: number, label?: string, sort?: string, type: string}, idCampaingn: string) {
-        try{
-            const response = await apiClient(APIContentType.JSON).get(`${END_POINT}/${idCampaingn}/groups`, {
-                params: query
-            });
-
-            if(!response || !response.data || response === undefined){
-                throw new Error("Invalid API response");
-            }
-
-            return response.data;
-        }catch(error){
-            console.error("Error fetching groups:", error);
-            return [];
-        }
+  async getAllGroups(
+    query: {
+      page?: number;
+      offset?: number;
+      label?: string;
+      sort?: string;
+      type: string;
     },
-
-    async updateCharacter(id: string, data: Partial<IGroup>) {
-        try {
-            const response = await apiClient(APIContentType.JSON).patch(`/groups/${id}`, data);
-      
-            if (!response || !response.data || response === undefined) {
-              throw new Error("Invalid API response");
-            }
-      
-            return response.data;
-        } catch (err: any) {
-            console.error("API error:", err);
-            return "error";
+    idCampaingn: string
+  ) {
+    try {
+      const response = await apiClient(APIContentType.JSON).get(
+        `${END_POINT}/${idCampaingn}/groups`,
+        {
+          params: query,
         }
-    }
+      );
 
-}
+      if (!response || !response.data || response === undefined) {
+        throw new Error("Invalid API response");
+      }
+
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching groups:", error);
+      return [];
+    }
+  },
+  async updateGroup(id: string, data: Partial<any>) {
+    try {
+      const response = await apiClient(APIContentType.JSON).patch(
+        `/groups/${id}`,
+        data
+      );
+
+      if (!response || !response.data || response === undefined) {
+        throw new Error("Invalid API response");
+      }
+
+      return response.data;
+    } catch (err: any) {
+      console.error("API error:", err);
+      return "error";
+    }
+  },
+};
 
 export default GroupService;
