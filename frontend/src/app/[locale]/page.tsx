4609fa2f4edc5760c8da263ff4874131f3316540
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-15 11:51:45 +0200

    Change locale switcher position

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import { Header } from "@/components/common/Header";
 import LocaleSwitcher from "@/components/locale/LocaleSwitcher";
 
 export default function Home() {
 
   return (
     <div className="flex flex-col">
-      <LocaleSwitcher />
+      <Header campaign={null} />
     </div>
   );
 }
