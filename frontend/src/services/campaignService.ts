c1b3802459c20e91360e8b55d882ba48082c5008
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-15 09:51:34 +0200

    Détails d'une campagne
    * Campagin details
    
    * Add trad

--- frontend/src/services/campaignService.ts
+++ frontend/src/services/campaignService.ts
@@ -25,6 +25,21 @@ const CampaignService = {
       return "error";
     }
   },
+
+  async updateCampaign(id: string, data: any) {
+    try {
+      const response = await apiClient(APIContentType.JSON).patch(`${moduleUrl}/${id}`, data);
+
+      if (!response || !response.data || response === undefined) {
+        throw new Error("Invalid API response");
+      }
+
+      return response.data;
+    } catch (err: any) {
+      console.error("API error:", err);
+      return "error";
+    }
+  }
 };
 
 export default CampaignService;
