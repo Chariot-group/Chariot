9da9013080debcf65e17c4aa4e018972ff478f2e
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-15 09:51:34 +0200

    Détails d'une campagne
    * Campagin details
    
    * Add trad

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -3,6 +3,38 @@ import LocaleSwitcher from "@/components/locale/LocaleSwitcher";
 import { useState } from "react";
 import CampaignListPanel from "@/components/modules/campaigns/CampaignListPanel";
 import { ICampaign } from "@/models/campaigns/ICampaign";
+<<<<<<< HEAD
+import { CharacterDetailsPanel } from "@/components/modules/characters/panelSections/CharacterDetailsPanel";
+import CampaignDetailsPanel from "@/components/modules/campaigns/CampaignDetailsPanel";
+
+export default function Home() {
+  const [group, setGroupSelected] = useState<IGroup | null>(null);
+
+  const [character, setCharacterSelected] = useState<ICharacter | null>(null);
+  const [selectedCampaign, setSelectedCampaign] = useState<ICampaign | null>(null);
+
+  return (
+    <div className="flex flex-col">
+      <LocaleSwitcher />
+      <div className="flex flex-row">
+        <div className="w-1/4 h-[100vh] bg-card">
+          <CampaignListPanel
+            offset={15}
+            selectedCampaign={selectedCampaign}
+            setSelectedCampaign={setSelectedCampaign}
+          />
+        </div>
+        {
+          selectedCampaign && (
+            <div className="w-1/4 h-[100vh] w-full">
+              <CampaignDetailsPanel
+                campaign={selectedCampaign}
+                setCampaign={setSelectedCampaign}
+              />
+            </div>
+          )}
+      </div>
+=======
 import { Header } from "@/components/common/Header";
 
 export default function Home() {
@@ -23,6 +55,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+>>>>>>> origin/develop
     </div>
   );
 }
