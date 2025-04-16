f35002c637613369e3897fa9634e6079f2c01e48
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-16 16:03:09 +0200

    Debut

---
+++ frontend/src/app/[locale]/campaigns/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/common/Header";
+
+export default function CampaignsPage() {
+    return (
+        <div className="w-full flex flex-col">
+            <Header campaign={null} />
+            <main className="h-full flex flex-row">
+                
+            </main>
+        </div>
+    );
+}
