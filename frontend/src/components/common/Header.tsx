530264daf6de2703c85351e841cd76492f7ae324
Elvis Pichou <elvis@MacBook-Pro-de-Pichou.local>
2025-04-17 11:50:51 +0200

    🐛 Fix(Header): Update battle link to use dynamic campaignId

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -4,21 +4,26 @@ import { ICampaign } from "@/models/campaigns/ICampaign";
 import { useEffect } from "react";
 import Link from "next/link";
 import LocaleSwitcher from "../locale/LocaleSwitcher";
+import { useParams } from "next/navigation";
 
 interface HeaderProps {
   campaign: ICampaign | null;
 }
 export function Header({ campaign }: HeaderProps) {
   const t = useTranslations("Header");
+  const { campaignId } = useParams();
 
   return (
     <header className="text-white p-4 bg-card border-b-2 border-ring shadow-md">
       <div className="flex justify-between items-center">
-        <Button>
-          <Link href={"/campaigns/67fcb61e1e90f27ba2e07a2b/battle"}>
-            {t("launchBattle")}
-          </Link>
-        </Button>
+        {campaignId && (
+          <Button>
+            <Link href={`/campaigns/${campaignId}/battle`}>
+              {t("launchBattle")}
+            </Link>
+          </Button>
+        )}
+        {!campaignId && <div className="w-1/12"></div>}
         <h1 className="text-foreground text-2xl font-bold">{`${t("home")} ${
           campaign ? `- ${campaign.label}` : ""
         }`}</h1>
