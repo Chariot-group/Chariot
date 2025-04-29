5a1b85294a24967deed76f9bfa0b81d880366440
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-04-29 13:45:15 +0200

    Reset mot de passe
    * Save OTP
    
    * Send email
    
    * Change

---
+++ backend/src/mailling/mailling.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { MaillingService } from './mailling.service';
+
+@Module({
+    providers: [MaillingService],
+    exports: [MaillingService],
+})
+export class MaillingModule {}
---
+++ backend/src/mailling/mailling.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MaillingService } from './mailling.service';
+
+describe('MaillingService', () => {
+  let service: MaillingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MaillingService],
+    }).compile();
+
+    service = module.get<MaillingService>(MaillingService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
