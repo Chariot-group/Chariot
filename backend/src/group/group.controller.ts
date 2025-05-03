bc41ba3eb17aa6e6148f4d4bfea7f9db5a504e94
Elvis Pichou <elvis@mac.home>
2025-05-03 19:32:13 +0200

    🔒️ Fix(groups): Add iscreator guard to the delete findone and update methods

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { GroupService } from '@/group/group.service';
 import { CreateGroupDto } from '@/group/dto/create-group.dto';
 import { UpdateGroupDto } from '@/group/dto/update-group.dto';
 import { ParseNullableIntPipe } from '@/pipes/parse-nullable-int.pipe';
 import { CharacterService } from '@/character/character.service';
+import { IsCreatorGuard } from '@/common/guards/is-creator.guard';
+import { IsCreator } from '@/common/decorators/is-creator.decorator';
 
+@UseGuards(IsCreatorGuard)
 @Controller('groups')
 export class GroupController {
   constructor(
@@ -40,16 +54,19 @@ export class GroupController {
     return this.characterService.findAll({ page, offset, name, sort }, id);
   }
 
+  @IsCreator(GroupService)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.groupService.findOne(id);
   }
 
+  @IsCreator(GroupService)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
     return this.groupService.update(id, updateGroupDto);
   }
 
+  @IsCreator(GroupService)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.groupService.remove(id);
