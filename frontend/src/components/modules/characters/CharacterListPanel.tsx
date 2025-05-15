a70ec064ef2df9cd53aae4c8a1480f29b765e91b
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-05-15 19:34:31 +0200

    Refacto structure front
    * Restructuration
    
    * Rmove cette enculé

--- frontend/src/components/modules/characters/CharacterListPanel.tsx
+++ frontend/src/components/modules/characters/CharacterListPanel.tsx
@@ -7,7 +7,7 @@ import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import { useToast } from "@/hooks/useToast";
 import ICharacter from "@/models/characters/ICharacter";
 import { IGroup } from "@/models/groups/IGroup";
-import CharacterService from "@/services/CharacterService";
+import CharacterService from "@/services/characterService";
 import { Plus, PlusCircleIcon } from "lucide-react";
 import { useLocale, useTranslations } from "next-intl";
 import Link from "next/link";
