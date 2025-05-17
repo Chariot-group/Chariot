618908ec26496de299b0d4f76131ba764b41671d
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-05-17 14:05:45 +0200

    Fix erreurs 401 en front
    * Fix redirect
    
    * Fix

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -36,7 +36,7 @@ export function Header({ campaign }: HeaderProps) {
             console.error("Error fetching user:", err);
         }
     },
-    []
+    [router]
   );
 
   const logout = () => {
