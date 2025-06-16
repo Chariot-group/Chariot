f4643fa6fad5d7781e6bfbe3454b0b38b70ca215
Elvis Pichou <56622967+Eyvos@users.noreply.github.com>
2025-06-16 20:15:45 +0200

    Fix langage switch
    * ♻️ Refactor(i18n): Integrate NextIntlClientProvider and message retrieval in LocaleLayout
    
    * 🐛 Fix(i18n): Add localized NotFound page with improved layout and translations

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from 'next';
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
 import { Geist, Geist_Mono } from 'next/font/google';
 import "./globals.css";
 
@@ -31,16 +29,13 @@ export default async function RootLayout({
 }) {
     const { locale } = await params;
 
-    const messages = await getMessages();
 
     return (
         <html
             lang={locale}
             suppressHydrationWarning={true}>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <NextIntlClientProvider messages={messages}>
                     {children}
-                </NextIntlClientProvider>
             </body>
         </html>
     );
