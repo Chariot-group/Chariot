15c2534c37c36b17940cf667bb1fff66ae06679d
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-06-18 10:22:06 +0200

    Ajout aide et automatisation
    * Support shift + Enter
    
    * Change trad
    
    * Maitrise Skill
    
    * Maitrise skill
    
    * Mastery ability
    
    * Select alignment
    
    * Fix trad and imports
    
    * 🐛 Fix(player): Change import from relative to absolute
    
    ---------
    
    Co-authored-by: Eyvos <elvis.pichou@gmail.com>

--- backend/src/resources/character/player/schemas/stats/player-stats.schema.ts
+++ backend/src/resources/character/player/schemas/stats/player-stats.schema.ts
@@ -1,10 +1,18 @@
 import { Stats } from '@/resources/character/core/schemas/stats/stats.schema';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Masteries } from '@/resources/character/player/schemas/stats/player-masteries.schema';
+import { MasteriesAbility } from '@/resources/character/player/schemas/stats/player-masteriesAbility';
 
 @Schema({ _id: false })
 export class PlayerStats extends Stats {
   @Prop({ default: 0 })
   proficiencyBonus: number;
+
+  @Prop({ type: Masteries, default: {} })
+  masteries: Masteries;
+
+  @Prop({ type: MasteriesAbility, default: {} })
+  masteriesAbility: MasteriesAbility;
 }
 
 export const PlayerStatsSchema = SchemaFactory.createForClass(PlayerStats);
