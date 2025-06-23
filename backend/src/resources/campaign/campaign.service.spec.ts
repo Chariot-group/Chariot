c479a080030b287a27594965690329d19360c5b9
Hugo Piedanna <hugop@MacBook-Pro-de-Hugo.local>
2025-06-23 14:40:50 +0200

    reset

--- backend/src/resources/campaign/campaign.service.spec.ts
+++ backend/src/resources/campaign/campaign.service.spec.ts
@@ -1,11 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CampaignService } from './campaign.service';
-import { getModelToken } from '@nestjs/mongoose';
-import { Campaign } from './schemas/campaign.schema';
-import { Group } from '@/resources/group/schemas/group.schema';
-import { CreateCampaignDto } from './dto/create-campaign.dto';
-import { UpdateCampaignDto } from './dto/update-campaign.dto';
-import { Types } from 'mongoose';
 
 describe('CampaignService', () => {
   let service: CampaignService;
