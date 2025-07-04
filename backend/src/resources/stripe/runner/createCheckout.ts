c14c192a68e6630657c3c0c579701e022b819e14
Hugo Piedanna <hugop@Van-DGSI.local>
2025-07-04 18:41:18 +0200

    Add docs

---
+++ backend/src/resources/stripe/runner/createCheckout.ts
@@ -0,0 +1,44 @@
+import Stripe from 'stripe';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+async function bootstrap() {
+
+    dotenv.config({ path: path.resolve(__dirname, '../../../../../.env') });
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: '2025-05-28.basil',
+    });
+
+    const session = await stripe.checkout.sessions.create(
+        {
+            mode: "subscription",
+            billing_address_collection: "required",
+            line_items: [
+                {
+                    price: "price_1ReH7CQCTCYypKV7WGEcZMPe",
+                    quantity: 1,
+                },
+            ],
+            success_url: `http://localhost:3000/success/{CHECKOUT_SESSION_ID}`,
+            cancel_url: `http://localhost:3000/cancel`,
+            custom_fields: [
+                {
+                    key: "pseudo",
+                    label: {
+                        type: "custom",
+                        custom: "Pseudo",
+                    },
+                    type: "text",
+                    text: {
+                        minimum_length: 1,
+                        maximum_length: 50,
+                    },
+                    optional: false,
+                },
+            ],
+        });
+
+    console.log('👉 URL Checkout:', session.url);
+}
+
+bootstrap();
